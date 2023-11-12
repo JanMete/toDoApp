7502147c38f36c3c4ef0b920ca318a0e690e27dc
Jan Metelański <metelanskij@gmail.com>
2023-11-12 16:21:25 +0100

    mediaQuery update

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -4,17 +4,26 @@ import { Button } from '../Button/Button';
 const StyledLi = styled.li`
   list-style-type: none;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   gap: 8px;
   border-top: 1px solid black;
   margin-top: 16px;
   padding-top: 16px;
   min-width: 400px;
+  @media screen and (max-width: 600px) {
+    max-width: 100vw;
+    min-width: 0;
+  }
 `;
 
 const StyledSpan = styled.span`
-  width: 100%;
+  /* width: 100%; */
   text-decoration: ${({ $done }) => ($done ? 'line-through' : '')};
+`;
+
+const StyledButtonContainer = styled.div`
+  display: flex;
 `;
 
 export function TodoItem({
@@ -26,8 +35,10 @@ export function TodoItem({
   return (
     <StyledLi>
       <StyledSpan $done={done}>{name}</StyledSpan>
-      {!done && <Button onClick={onDoneButtonClick}>Zrobione</Button>}
-      <Button onClick={onDeleteButtonClick}>Usuń</Button>
+      <StyledButtonContainer className='buttonContainer'>
+        {!done && <Button onClick={onDoneButtonClick}>Zrobione</Button>}
+        <Button onClick={onDeleteButtonClick}>Usuń</Button>
+      </StyledButtonContainer>
     </StyledLi>
   );
 }
